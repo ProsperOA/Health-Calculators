a3ce1abfd8843660279049131a90bfc8ddff80b4
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2016-11-24 15:13:17 -0800

    remove params, add timeout to avoid null data

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -1,10 +1,26 @@
 // Login Page
 
 angular.module('healthCalculators')
-  .controller('MainCtrl', function ($scope, $rootScope, $location, $window, loginFactory) {
-    
+  .controller('MainCtrl', function ($scope, $rootScope, $timeout, loginFactory) {
+
+    $scope.user = loginFactory.getUser();
+    $scope.env = loginFactory.getEnv();
+
     $scope.signIn = function() {
       loginFactory.signIn();
     };
+    $scope.signOut = function() {
+      loginFactory.signOut();
+    };
+
+    loginFactory.registerObserverCallback(
+      function() {
+        $timeout(function() {
+          $scope.user = loginFactory.getUser();
+          $scope.env = loginFactory.getEnv();
+          $scope.$apply();
+        });
+      }
+    );
 
 });
