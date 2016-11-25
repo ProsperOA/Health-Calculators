641ebb16a287d91abb1fcacf31a34c0735a600a6
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2016-11-24 17:35:10 -0800

    remove login page references

--- www/js/app.js
+++ www/js/app.js
@@ -37,16 +37,6 @@ angular.module('healthCalculators', ['ionic', 'healthCalculators.controllers'])
     }
   })
 
-  .state('app.login', {
-    url: '/login',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/login.html',
-        controller: 'LoginCtrl'
-      }
-    }
-  })
-
   .state('app.bmi-calculator', {
     url: '/bmi-calculator',
     views: {
