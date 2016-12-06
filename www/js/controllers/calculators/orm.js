e372247954261787ce87018906cb6db3d0f39586
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2016-12-05 20:37:49 -0800

    remove js validation, clean code, add comments

--- www/js/controllers/calculators/orm.js
+++ www/js/controllers/calculators/orm.js
@@ -1,36 +1,33 @@
-// ORM Calculator Controller
+// One Repetition Maximum Calculator
 
 angular.module('healthCalculators')
-.controller('OrmCtrl', function ($scope, globals) {
+.controller('OrmCtrl', function($scope, globals) {
     $scope.calcOrm = function() {
-      var $info = $scope.user.info;
-      
-      if(isNaN($info.weight_lifted, $info.reps))
-        return;
-      
-      var orm = $info.weight_lifted / (1.0278 - (0.0278 * $info.reps));
-      
-      $info.orm = $info.orm_1Reps = Math.round(orm) + globals.lbs;
-      // Percentages & Reps
-      $info.orm_95 = $info.orm_2Reps = Math.round(orm * 0.95) + globals.lbs;
-      $info.orm_90 = $info.orm_3Reps = Math.round(orm * 0.90) + globals.lbs;
-      $info.orm_85 = Math.round(orm * 0.85) + globals.lbs;
-      $info.orm_80 = $info.orm_7Reps = Math.round(orm * 0.80) + globals.lbs;
-      $info.orm_75 = $info.orm_10Reps = Math.round(orm * 0.75) + globals.lbs;
-      $info.orm_70 = $info.orm_12Reps = $info.orm_7Reps = Math.round(orm * 0.70) + globals.lbs;
-      $info.orm_65 = Math.round(orm * 0.65) + globals.lbs;
-      $info.orm_60 = Math.round(orm * 0.60) + globals.lbs;
-      
-      // Reps
-      $info.orm_4Reps = Math.round(orm * 0.88) + globals.lbs;
-      $info.orm_5Reps = Math.round(orm * 0.86) + globals.lbs;
-      $info.orm_6Reps = Math.round(orm * 0.83) + globals.lbs;
-      $info.orm_8Reps = Math.round(orm * 0.78) + globals.lbs;
-      $info.orm_9Reps = Math.round(orm * 0.76) + globals.lbs;
-      $info.orm_11Reps = Math.round(orm * 0.72) + globals.lbs;
-      $info.orm_15Reps = Math.round(orm * 0.66) + globals.lbs;
-      $info.orm_20Reps = Math.round(orm * 0.60) + globals.lbs;
-      
+        var $info = $scope.user.info;
+
+        var orm = $info.weight_lifted / (1.0278 - (0.0278 * $info.reps));
+
+        $info.orm = $info.orm_1Reps = Math.round(orm) + globals.lbs;
+        // Percentages & Reps
+        $info.orm_95 = $info.orm_2Reps = Math.round(orm * 0.95) + globals.lbs;
+        $info.orm_90 = $info.orm_3Reps = Math.round(orm * 0.90) + globals.lbs;
+        $info.orm_85 = Math.round(orm * 0.85) + globals.lbs;
+        $info.orm_80 = $info.orm_7Reps = Math.round(orm * 0.80) + globals.lbs;
+        $info.orm_75 = $info.orm_10Reps = Math.round(orm * 0.75) + globals.lbs;
+        $info.orm_70 = $info.orm_12Reps = $info.orm_7Reps = Math.round(orm * 0.70) + globals.lbs;
+        $info.orm_65 = Math.round(orm * 0.65) + globals.lbs;
+        $info.orm_60 = Math.round(orm * 0.60) + globals.lbs;
+
+        // Repetitions
+        $info.orm_4Reps = Math.round(orm * 0.88) + globals.lbs;
+        $info.orm_5Reps = Math.round(orm * 0.86) + globals.lbs;
+        $info.orm_6Reps = Math.round(orm * 0.83) + globals.lbs;
+        $info.orm_8Reps = Math.round(orm * 0.78) + globals.lbs;
+        $info.orm_9Reps = Math.round(orm * 0.76) + globals.lbs;
+        $info.orm_11Reps = Math.round(orm * 0.72) + globals.lbs;
+        $info.orm_15Reps = Math.round(orm * 0.66) + globals.lbs;
+        $info.orm_20Reps = Math.round(orm * 0.60) + globals.lbs;
+
     };
-    
+
 });
