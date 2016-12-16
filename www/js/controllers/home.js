476c316515d8568dfd5693a323e26f6d9dd41a3e
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2016-12-15 23:19:54 -0800

    add login modal layout

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -1,7 +1,20 @@
 // BMR Calculator Controller
 
 angular.module('healthCalculators')
-.controller('HomeCtrl', function ($scope, loginFactory) {
-  
+.controller('HomeCtrl', function ($scope, $ionicModal, loginFactory) {
+
+  // Login Modal
+  $ionicModal.fromTemplateUrl('loginModal', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
 
 });
